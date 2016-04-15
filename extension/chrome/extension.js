5d5ebe92dd011318fa52ad918ec1daf15b67cb97
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-04-15 09:48:34 +0100

    more child friendlyness, also removed extension (can just bookmark /go)

--- extension/chrome/extension.js
+++
@@ -1,9 +0,0 @@
-// Let's the procrastination commence (from extension).
-function procrastinate() {
-    if (!isLoaded)
-        loadPlaces(true);
-    else if (places) {
-        var indexChosen = Math.floor(Math.random() * places.length);
-        chrome.tabs.create({ url: places[indexChosen] });
-    }
-}
